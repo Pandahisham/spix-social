92fbc387352e5147967ce0138f0ae6260f43ea05
Andre Moreira <mourdok@gmail.com>
2014-04-07 08:31:39 -0300

    Add support to Like on Comments

--- app/classes/CommentsHelper.php
+++ app/classes/CommentsHelper.php
@@ -18,11 +18,23 @@ class CommentsHelper {
 
 		// Dump!
 		foreach ($comments as $comment) {
+
 			echo '<hr />';
 			echo '<blockquote>';
+
+			// Comment
 			echo '<p>'.$comment->body.'</p>';
 			echo '<footer>'.$comment->user->email.', '.$comment->created_at.'</footer>';
+
+			// Like
+			echo '<br />';
+			echo Form::open(array('url'=>'likes'));
+			echo Form::hidden('comment_id', $comment->id);
+			echo Form::submit('Like', array('class'=>'btn btn-default'));
+			echo Form::close();
+
 			echo '</blockquote>';
+
 		}
 
 	}
